fd81ec33219e47a8ceb10d9102220e106ef7ec36
henry232323 <henry@henrysmodpacks.com>
2017-10-24 21:36:41 -0600

    I realize now that __all__ is superfluous

--- henrio/__init__.py
+++ henrio/__init__.py
@@ -1,27 +1,15 @@
-all = ["get_default_loop", "run", "run_forever", "CancelledError"]
-from .futures import Future, Task, timeout, __all__
-all.extend(__all__)
-
+from .futures import Future, Task, timeout
 from .yields import (sleep, get_loop, unwrap_file, create_reader, create_writer, remove_reader,
                      remove_writer, spawn, wrap_file, wrap_socket, socket_connect, socket_bind, current_task,
-                     unwrap_socket, postpone, spawn_after, __all__)
-all.extend(__all__)
-
-from .bases import AbstractLoop, BaseFile, BaseSocket, AbstractProtocol, IOBase, __all__
-all.extend(__all__)
-from .locks import Lock, ResourceLock, __all__
-all.extend(__all__)
-from .loop import BaseLoop, __all__
-all.extend(__all__)
+                     unwrap_socket, postpone, spawn_after)
+from .bases import AbstractLoop, BaseFile, BaseSocket, AbstractProtocol, IOBase
+from .locks import Lock, ResourceLock
+from .loop import BaseLoop
 from .protocols import ConnectionBase, connect, create_server, ServerBase, ssl_connect, SSLServer, create_ssl_server, \
-    ServerSocket, __all__
-all.extend(__all__)
-from .queue import Queue, __all__
-all.extend(__all__)
-from .selector import SelectorLoop, SelectorFile, SelectorSocket, __all__
-all.extend(__all__)
-from .workers import worker, async_worker, __all__
-all.extend(__all__)
+    ServerSocket
+from .queue import Queue
+from .selector import SelectorLoop, SelectorFile, SelectorSocket
+from .workers import worker, async_worker
 
 import concurrent.futures
 CancelledError = concurrent.futures.CancelledError
@@ -29,8 +17,7 @@ del concurrent
 
 import sys
 if sys.platform == "win32":
-    from .windows import IOCPLoop, IOCPFile, IOCPSocket, IOCPInstance, __all__
-    all.extend(__all__)
+    from .windows import IOCPLoop, IOCPFile, IOCPSocket, IOCPInstance
 
 del sys  # Not for export
 
@@ -49,7 +36,3 @@ def run_forever(*awaitables):
     for awaitable in awaitables:
         loop.create_task(awaitable)
     return loop.run_forever()
-
-
-__all__ = all
-del all
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='henrio',
-    version='2.0',
+    version='3.0',
     description='My trials screwing with async loops',
     url='https://github.com/henry232323/henrio',
     author='henry232323',

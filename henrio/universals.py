a4a0f50b12994f142b08e2f24efc450664a19adb
henry232323 <henry@henrysmodpacks.com>
2017-12-14 22:32:20 -0700

    New universals from my async-recipes

--- henrio/universals.py
+++ henrio/universals.py
@@ -8,12 +8,13 @@ from . import Future, Task, Conditional
 from . import Queue, HeapQueue
 from . import sleepinf
 
-from types import coroutine
 import typing
 from math import inf
 from time import monotonic
+import select
+from types import coroutine
 
-__all__ = ["Future", "Task", "Conditional", "Queue", "HeapQueue", "sleepinf", "sleep"]
+__all__ = ["Future", "Task", "Conditional", "Queue", "HeapQueue", "sleepinf", "sleep", "timeout", "AsyncFile"]
 
 
 @coroutine
@@ -27,3 +28,75 @@ def sleep(seconds: typing.Union[float, int]):
         end = monotonic() + seconds
         while end >= monotonic():
             yield
+
+
+class timeout:
+    def __init__(self, coro, timeout):
+        if hasattr(coro, "__await__"):
+            self._coro = coro.__await__()
+        else:
+            self._coro = coro
+        self._finish = monotonic() + timeout
+
+    def __iter__(self):
+        try:
+            val = None
+            while monotonic() < self._finish:
+                val = yield self._coro.send(val)
+            else:
+                raise TimeoutError
+        except StopIteration as err:
+            return err.value
+
+    __await__ = __iter__
+
+    def send(self, data):
+        if monotonic() < self._finish:
+            return self._coro.send(data)
+        raise TimeoutError
+
+    def throw(self, data):
+        if monotonic() < self._finish:
+            return self._coro.throw(data)
+        raise TimeoutError
+
+
+class AsyncFile:
+    def __init__(self, file):
+        self.file = file
+
+    @coroutine
+    def recv(self, nbytes):
+        cond = Conditional(lambda: select.select([self.file], [], [], 0)[0])
+        yield from cond
+        return self.file.recv(nbytes)
+
+    @coroutine
+    def read(self, nbytes):
+        cond = Conditional(lambda: select.select([self.file], [], [], 0)[0])
+        yield from cond
+        return self.file.read(nbytes)
+
+    @coroutine
+    def send(self, data):
+        cond = Conditional(lambda: select.select([], [self.file], [], 0)[1])
+        yield from cond
+        return self.file.send(data)
+
+    @coroutine
+    def write(self, data):
+        cond = Conditional(lambda: select.select([], [self.file], [], 0)[1])
+        yield from cond
+        return self.file.write(data)
+
+    @coroutine
+    def accept(self):
+        cond = Conditional(lambda: select.select([self.file], [], [], 0)[0])
+        yield from cond
+        return self.file.accept()
+
+    def fileno(self):
+        return self.file.fileno()
+
+    def close(self):
+        self.file.close()
